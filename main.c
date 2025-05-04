4d8fc5208060fabe17cbf8b9bb6971d84717dc3d
Proxy539 <roman.filiyutich@gmail.com>
2025-05-04 14:22:22 +0300

    init commit

--- main.c
+++ main.c
@@ -1,9 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
+typedef struct {
+    char *name;
+    int number;
+} Task;
+
+int main(int argc, char *argv[]) {
 
 
-    printf("Hello CLI app!\n");
+    if (argc == 1) {
+        printf("Usage: <command>\n");
+        exit(EXIT_FAILURE);
+    }
+
+
+    FILE *file = fopen("tasks.txt", "a+");
+    
+    if (!file) {
+        perror("Can't open the file");
+        exit(EXIT_FAILURE);
+    }
+
+    if (strcmp(argv[1], "list") == 0) {
+        listTasks(file);
+    } else if(strcmp(argv[1], "add") == 0) {
+
+        if (argc == 2) {
+            printf("Usage: add <taskName>\n");
+        } else {
+            addTask(file, argv[2]);
+        }
+
+    } else if (strcmp(argv[1], "done") == 0) {
+        if (argc == 2) {
+            printf("Usage: done <taskNumber>\n");
+        } else {
+            doneTask(file, argv[2]);
+        } 
+    } else if (strcmp(argv[1], "delete") == 0) {
+
+        if (argc == 2) {
+            printf("Usage: delete <taskNumber>"); 
+        } else {
+            deleteTask(file, argv[2]);
+        }
+        
+    }
+
+
+    if (!file) {
+        printf("tasks.txt closed successfully\n");
+    } else {
+        printf("Can't close tasks.txt file\n");
+    }
+
 
 
     return 0;
